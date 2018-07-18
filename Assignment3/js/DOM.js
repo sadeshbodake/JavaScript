1d0f3211a03590be14676d5a46349b803f54f01f
kajalpkamble <sandesh.bodake@wgblindia.com>
2018-07-18 20:41:19 +0530

    Assignment Updated...

--- Assignment3/js/DOM.js
+++ Assignment3/js/DOM.js
@@ -1,68 +1,84 @@
-var toggle=(function(){
-  var counter=0;
+js = {
+  settings:{
+    capture:document.getElementById("capture"),
+    switchboard:document.getElementsByClassName("switchboard"),
+    switchbtn:document.getElementsByClassName("btn"),
+    text:document.getElementById('text'),
+    msg:document.getElementById('msg'),
+    block:document.getElementsByClassName('block'),
+    counter:0,
+    timer:5000,
+  },
 
-  return function(){
+  init: function() {
+    s=this.settings
+    this.bindUIActions();
+  },
 
-    if(document.getElementById("capture").checked==true){
-      if(counter%2==0){
-        document.getElementsByClassName("btn")[0].style.backgroundColor="yellow"
-        document.getElementById('text').innerHTML="ON";
-        addmsg()
-        colorBlock()
+  bindUIActions: function() {
+    s.switchbtn[0].addEventListener("click", function() {
+
+      if(s.capture.checked==true){
+        if(s.counter%2==0){
+          s.switchbtn[0].style.backgroundColor="yellow"
+
+          s.text.innerHTML="ON";
+          js.addmsg()
+          js.colorBlock()
+        }
+        else{
+          s.switchbtn[0].style.backgroundColor="gray"
+          s.text.innerHTML="OFF";
+          js.addmsg()
+          js.reveseColor()
+        }
       }
-
       else{
-        document.getElementsByClassName("btn")[0].style.backgroundColor="gray"
-        document.getElementById('text').innerHTML="OFF";
-        addmsg()
-        reveseColor()
-      }
-    }
-    else{
-      if(counter%2==0){
-        document.getElementsByClassName("switchboard")[0].style.backgroundColor="green"
-        document.getElementById('text').innerHTML="ON";
-        addmsg()
-        colorBlock()
-      }
-      else{
-        document.getElementsByClassName("switchboard")[0].style.backgroundColor="red"
-        document.getElementById('text').innerHTML="OFF";
-        addmsg()
-        reveseColor()
-      }
-    }
-    counter++;
-  }
 
-  function addmsg(){
-    document.getElementById('msg').innerHTML="Hello Sandesh.."
+        if(s.counter%2==0){
+          s.switchboard[0].style.backgroundColor="green"
+          text.innerHTML="ON";
+          js.addmsg()
+          js.colorBlock()
+        }
+        else{
+          s.switchboard[0].style.backgroundColor="red"
+          text.innerHTML="OFF";
+          js.addmsg()
+          js.reveseColor()
+        }
+      }
+      s.counter++;
+    });
+  },
+
+  addmsg:function(){
+    s.msg.innerHTML="Hello Sandesh.."
     setTimeout(function(){
-      document.getElementById('msg').innerHTML="";
-    },5000)
-  }
+      s.msg.innerHTML="";
+    },s.timer)
+  },
 
-  function colorBlock(){
+  colorBlock:function(){
     for(var i=0;i<4;i++){
       if(i%2==0){
-        document.getElementsByClassName('block')[i].style.backgroundColor="red";
+        s.block[i].style.backgroundColor="red";
       }
       else{
-        document.getElementsByClassName('block')[i].style.backgroundColor="black";
+        s.block[i].style.backgroundColor="black";
       }
     }
-  }
+  },
 
-  function reveseColor(){
+  reveseColor:function(){
     for(var i=0;i<4;i++){
       if(i%2!=0){
-        document.getElementsByClassName('block')[i].style.backgroundColor="red";
+        s.block[i].style.backgroundColor="red";
       }
       else{
-        document.getElementsByClassName('block')[i].style.backgroundColor="black";
+        s.block[i].style.backgroundColor="black";
       }
     }
   }
-
-})();
-
+};
+js.init()
